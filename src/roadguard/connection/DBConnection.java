ede57f75140c8933a8e67a7fe7766f70b3de134a
Aman <amanraj06012006@gmail.com>
2026-08-24 22:09:28 +0530

    Add MySQL JDBC connection setup

---
+++ src/roadguard/connection/DBConnection.java
@@ -0,0 +1,22 @@
+package roadguard.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/road_accident";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Aman@0601";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Database connection failed!");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
